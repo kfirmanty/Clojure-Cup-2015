e043527833425a0050d3b03b21176ed327b24be1
Marcin Gasperowicz <xnooga@gmail.com>
2015-12-05 19:52:52 +0100

    add ajax lib

--- synth/project.clj
+++ synth/project.clj
@@ -7,6 +7,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"]
+                 [cljs-ajax "0.5.2"]
                  [reagent "0.5.0"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
